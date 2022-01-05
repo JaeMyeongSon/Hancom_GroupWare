a8e753200574e00da4547fdf9752e7faa8d61d4b
jaemyeongson111 <jaemyeongson@kakao.com>
2022-01-05 17:18:19 +0900

    업로드

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 727
-  ClientWidth = 1206
+  ClientHeight = 529
+  ClientWidth = 669
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -17,12 +17,14 @@ object Form1: TForm1
   object WebBrowser1: TWebBrowser
     Left = 0
     Top = 0
-    Width = 1198
+    Width = 669
     Height = 486
+    Align = alTop
     TabOrder = 0
     OnDocumentComplete = WebBrowser1DocumentComplete
+    ExplicitWidth = 697
     ControlData = {
-      4C000000D17B00003B3200000000000000000000000000000000000000000000
+      4C000000254500003B3200000000000000000000000000000000000000000000
       000000004C000000000000000000000001000000E0D057007335CF11AE690800
       2B2E126208000000000000004C0000000114020000000000C000000000000046
       8000000000000000000000000000000000000000000000000000000000000000
@@ -33,7 +35,7 @@ object Form1: TForm1
     Top = 492
     Width = 75
     Height = 25
-    Caption = 'Button1'
+    Caption = #53748#44540#54616#44592
     TabOrder = 1
     OnClick = Button1Click
   end
@@ -44,11 +46,18 @@ object Form1: TForm1
     Height = 25
     Caption = 'Button2'
     TabOrder = 2
-    OnClick = Button2Click
   end
   object Timer1: TTimer
+    Enabled = False
     OnTimer = Timer1Timer
     Left = 600
+    Top = 368
+  end
+  object Timer2: TTimer
+    Enabled = False
+    Interval = 5000
+    OnTimer = Timer2Timer
+    Left = 640
     Top = 368
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -13,13 +13,14 @@ type
     Button1: TButton;
     Button2: TButton;
     Timer1: TTimer;
+    Timer2: TTimer;
     procedure Button1Click(Sender: TObject);
 
     procedure WebBrowser1DocumentComplete(ASender: TObject;
     const pDisp: IDispatch; const URL: OleVariant);
-    procedure Button2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
+    procedure Timer2Timer(Sender: TObject);
 
 
   private
@@ -37,31 +38,12 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
- //   WebBrowser1.Navigate('https://gw.hancom.com/ekp/view/login/userLogin');
-
-end;
-
-procedure TForm1.Button2Click(Sender: TObject);
-var
-  Buttons: OleVariant;
-  Button: OleVariant;
-  I: Integer;
-begin
-  Buttons := WebBrowser1.OleObject.Document.getElementsByTagName('button');
-  for I := 0 to Buttons.Length - 1 do
-  begin
-    Button := Buttons.item(I);
-    if Button.innerText = '로그인' then
-    begin
-      Button.click();
-      Break;
-    end;
-  end;
+WebBrowser1.Navigate('https://gw.hancom.com/ekp/view/login/userLogin');
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
- WebBrowser1.Navigate('https://gw.hancom.com/ekp/view/login/userLogin');
+WebBrowser1.Silent := True;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
@@ -77,6 +59,28 @@ begin
     if Button.innerText = '로그인' then
     begin
       Button.click();
+      timer2.Enabled :=true;
+      Break;
+
+      timer1.Enabled := false;
+    end;
+  end;
+end;
+
+procedure TForm1.Timer2Timer(Sender: TObject);
+var
+  Buttons: OleVariant;
+  Button: OleVariant;
+  I: Integer;
+begin
+  Buttons := WebBrowser1.OleObject.Document.getElementsByTagName('button');
+  for I := 0 to Buttons.Length - 1 do
+  begin
+    Button := Buttons.item(I);
+    if Button.innerText = '작성' then
+    begin
+      Button.click();
+      timer2.Enabled := false;
       Break;
     end;
   end;
@@ -93,36 +97,23 @@ var
   Button: OleVariant;
 begin
 
-//웹페이지의 로딩완료 이벤트에서 작업을 진행합니다.
-
-
+  Buttons := WebBrowser1.OleObject.Document.getElementsByTagName('button');
+  for I := 0 to Buttons.Length - 1 do
+  begin
+    Button := Buttons.item(I);
+    if Button.innerText = '로그인' then
+    begin
 
     doc3 := (pDisp as IWebBrowser2).Document as IHTMLDocument3;
-
-//Element를 찾기위해 Document를 IHTMLDocument3에 맞춰 가져옵
-
 
     doc3.getElementById('userId').setAttribute('value', 'jmson.fro', 0 );
     doc3.getElementById('password').setAttribute('value', 'epahseheh3A!', 0 );
 
-//가져온 Document에서 아이디, 비밀번호 입력창의 위치를 id로 찾아와 값을 넣어줍니다.
+    timer1.Enabled := true;
 
- timer1.Enabled := true;
+    end;
+  end;
 end;
-
-
-   { eleColl := doc3.getElementsByTagName('button');
-
-    for I := 0 to eleColl.length - 1 do
-    begin
-    if (eleColl.item(I, 0) as IHTMLInputElement).type_ = 'submit' then
-    (eleColl.item(I, 0) as IHTMLInputElement).form.submit;
-     end; }
-
-
-
-
-//Collection에서 로그인 버튼에 해당하는 Element를 찾아와 Submit을 실행시켜줍니다.
 
 
 end.
