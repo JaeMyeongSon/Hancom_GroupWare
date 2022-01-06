65dc03082799cec9cd6f7ed0a86bc892ad9c1d21
jaemyeongson111 <jaemyeongson@kakao.com>
2022-01-06 17:54:46 +0900

    업로드

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,9 +1,9 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
-  ClientHeight = 529
-  ClientWidth = 669
+  Caption = #54620#52980' '#52636#53748#44540
+  ClientHeight = 88
+  ClientWidth = 274
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,50 +14,102 @@ object Form1: TForm1
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 67
+    Width = 54
+    Height = 13
+    Caption = #44540#47924' '#49345#53468' :'
+  end
+  object Label2: TLabel
+    Left = 68
+    Top = 67
+    Width = 31
+    Height = 13
+    Caption = 'Label2'
+  end
   object WebBrowser1: TWebBrowser
-    Left = 0
-    Top = 0
-    Width = 669
-    Height = 486
-    Align = alTop
+    Left = 8
+    Top = 175
+    Width = 377
+    Height = 250
     TabOrder = 0
     OnDocumentComplete = WebBrowser1DocumentComplete
-    ExplicitWidth = 697
     ControlData = {
-      4C000000254500003B3200000000000000000000000000000000000000000000
+      4C000000F7260000D71900000000000000000000000000000000000000000000
       000000004C000000000000000000000001000000E0D057007335CF11AE690800
       2B2E126208000000000000004C0000000114020000000000C000000000000046
       8000000000000000000000000000000000000000000000000000000000000000
       00000000000000000100000000000000000000000000000000000000}
   end
   object Button1: TButton
-    Left = 40
-    Top = 492
-    Width = 75
-    Height = 25
-    Caption = #53748#44540#54616#44592
+    Left = 8
+    Top = 8
+    Width = 113
+    Height = 49
+    Caption = #52636#44540#54616#44592
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Constantia'
+    Font.Style = [fsBold]
+    ParentFont = False
     TabOrder = 1
     OnClick = Button1Click
   end
   object Button2: TButton
-    Left = 552
-    Top = 492
-    Width = 75
-    Height = 25
-    Caption = 'Button2'
+    Left = 153
+    Top = 8
+    Width = 112
+    Height = 49
+    Caption = #53748#44540#54616#44592
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Constantia'
+    Font.Style = [fsBold]
+    ParentFont = False
     TabOrder = 2
+    OnClick = Button2Click
+  end
+  object WebBrowser2: TWebBrowser
+    Left = 432
+    Top = 175
+    Width = 329
+    Height = 250
+    TabOrder = 3
+    OnDocumentComplete = WebBrowser2DocumentComplete
+    ControlData = {
+      4C00000001220000D71900000000000000000000000000000000000000000000
+      000000004C000000000000000000000001000000E0D057007335CF11AE690800
+      2B2E126208000000000000004C0000000114020000000000C000000000000046
+      8000000000000000000000000000000000000000000000000000000000000000
+      00000000000000000100000000000000000000000000000000000000}
   end
   object Timer1: TTimer
     Enabled = False
     OnTimer = Timer1Timer
-    Left = 600
-    Top = 368
+    Left = 112
+    Top = 416
   end
   object Timer2: TTimer
     Enabled = False
     Interval = 5000
     OnTimer = Timer2Timer
-    Left = 640
-    Top = 368
+    Left = 152
+    Top = 416
+  end
+  object Timer3: TTimer
+    Enabled = False
+    OnTimer = Timer3Timer
+    Left = 528
+    Top = 416
+  end
+  object Timer4: TTimer
+    Enabled = False
+    Interval = 5000
+    OnTimer = Timer4Timer
+    Left = 560
+    Top = 416
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -14,6 +14,11 @@ type
     Button2: TButton;
     Timer1: TTimer;
     Timer2: TTimer;
+    WebBrowser2: TWebBrowser;
+    Timer3: TTimer;
+    Timer4: TTimer;
+    Label1: TLabel;
+    Label2: TLabel;
     procedure Button1Click(Sender: TObject);
 
     procedure WebBrowser1DocumentComplete(ASender: TObject;
@@ -21,6 +26,11 @@ type
     procedure FormCreate(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure WebBrowser2DocumentComplete(ASender: TObject;
+      const pDisp: IDispatch; const URL: OleVariant);
+    procedure Timer3Timer(Sender: TObject);
+    procedure Timer4Timer(Sender: TObject);
 
 
   private
@@ -41,10 +51,17 @@ begin
 WebBrowser1.Navigate('https://gw.hancom.com/ekp/view/login/userLogin');
 end;
 
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+WebBrowser2.Navigate('https://gw.hancom.com/ekp/view/login/userLogin');
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
 WebBrowser1.Silent := True;
+WebBrowser2.Silent := True;
 end;
+
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 var
@@ -60,9 +77,8 @@ begin
     begin
       Button.click();
       timer2.Enabled :=true;
-      Break;
-
       timer1.Enabled := false;
+      Break;
     end;
   end;
 end;
@@ -77,10 +93,57 @@ begin
   for I := 0 to Buttons.Length - 1 do
   begin
     Button := Buttons.item(I);
-    if Button.innerText = '작성' then
+    if Button.innerText = '출근' then
     begin
       Button.click();
+      label2.caption := '출근';
       timer2.Enabled := false;
+
+      Break;
+
+    end;
+  end;
+end;
+
+
+
+procedure TForm1.Timer3Timer(Sender: TObject);
+var
+  Buttons: OleVariant;
+  Button: OleVariant;
+  I: Integer;
+begin
+  Buttons := WebBrowser1.OleObject.Document.getElementsByTagName('button');
+  for I := 0 to Buttons.Length - 1 do
+  begin
+    Button := Buttons.item(I);
+    if Button.innerText = '로그인' then
+    begin
+      Button.click();
+      timer4.Enabled :=true;
+       timer3.Enabled := false;
+      Break;
+
+    end;
+  end;
+end;
+
+
+procedure TForm1.Timer4Timer(Sender: TObject);
+var
+  Buttons: OleVariant;
+  Button: OleVariant;
+  I: Integer;
+begin
+  Buttons := WebBrowser1.OleObject.Document.getElementsByTagName('button');
+  for I := 0 to Buttons.Length - 1 do
+  begin
+    Button := Buttons.item(I);
+    if Button.innerText = '퇴근' then
+    begin
+      Button.click();
+      label2.caption := '퇴근';
+       timer4.Enabled := false;
       Break;
     end;
   end;
@@ -110,6 +173,35 @@ begin
     doc3.getElementById('password').setAttribute('value', 'epahseheh3A!', 0 );
 
     timer1.Enabled := true;
+
+    end;
+  end;
+end;
+
+procedure TForm1.WebBrowser2DocumentComplete(ASender: TObject;
+  const pDisp: IDispatch; const URL: OleVariant);
+var
+    doc3 : IHTMLDocument3;
+    eleColl : IHTMLElementCollection;
+    I: Integer;
+
+    Buttons: OleVariant;
+  Button: OleVariant;
+begin
+
+  Buttons := WebBrowser2.OleObject.Document.getElementsByTagName('button');
+  for I := 0 to Buttons.Length - 1 do
+  begin
+    Button := Buttons.item(I);
+    if Button.innerText = '로그인' then
+    begin
+
+    doc3 := (pDisp as IWebBrowser2).Document as IHTMLDocument3;
+
+    doc3.getElementById('userId').setAttribute('value', 'jmson.fro', 0 );
+    doc3.getElementById('password').setAttribute('value', 'epahseheh3A!', 0 );
+
+    //timer1.Enabled := true;
 
     end;
   end;
